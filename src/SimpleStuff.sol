90276ba3bd0ee0b3216ec3ee245325ce09eae455
9oelM <hj923@hotmail.com>
2024-12-03 11:34:29 +0900

    feat: minimal poc

--- src/SimpleStuff.sol
+++ src/SimpleStuff.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract SimpleStuff {
+import {InterchainTokenExecutable} from "interchain-token-service/executable/InterchainTokenExecutable.sol";
+import {InterchainTokenService} from "interchain-token-service/InterchainTokenService.sol";
+
+contract SimpleStuff is InterchainTokenExecutable {
+    event ExecuteWithInterchainToken(
+        bytes32 commandId,
+        string sourceChain,
+        bytes sourceAddress,
+        bytes data,
+        bytes32 tokenId,
+        address token,
+        uint256 amount
+    );
+    event DecodedData(bytes destinationAddress, string message);
+
     uint256 public number;
+    string immutable DESTINATION_CHAIN = "xrpl";
+
+    constructor(address _interchainTokenService) InterchainTokenExecutable(_interchainTokenService) {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -14,5 +31,32 @@ contract SimpleStuff {
 
     function receiveAndSendBack() public {
         increment();
+    }
+
+    function _executeWithInterchainToken(
+        bytes32 commandId,
+        string calldata sourceChain,
+        bytes calldata sourceAddress,
+        bytes calldata data,
+        bytes32 tokenId,
+        address token,
+        uint256 amount
+    ) internal virtual override {
+        emit ExecuteWithInterchainToken(commandId, sourceChain, sourceAddress, data, tokenId, token, amount);
+
+        (bytes memory destinationAddress, string memory message) = abi.decode(data, (bytes, string));
+
+        emit DecodedData(destinationAddress, message);
+
+        bytes memory replyData = abi.encodePacked(0x1234, "reply");
+
+        InterchainTokenService(interchainTokenService).callContractWithInterchainToken(
+            tokenId,
+            DESTINATION_CHAIN,
+            destinationAddress,
+            amount / 2, // uint256 amount,
+            replyData, // bytes memory data,
+            0 // uint256 gasValue
+        );
     }
 }
